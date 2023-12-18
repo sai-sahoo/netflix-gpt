32b8327b211157f96fbb2168b5264b2827350dc5
sai-sahoo <54710575+sai-sahoo@users.noreply.github.com>
2023-12-18 07:36:33 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOWI3YjlhYWY3MDE5NzZjYWMyZDkyNTZiNmQxY2YwMiIsInN1YiI6IjY0YjJiNWE2MjNkMjc4MDBjOTNiYjAyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iUnaqoYTL1u6bd41Kawfm8lJFU4BC3Ve9Bs5gzPER0A'
+        Authorization: 'XXXXXXXXXXX'
     }
 };
 export const TMDB_MOVIE_POSTER = 'https://image.tmdb.org/t/p/w500/';
