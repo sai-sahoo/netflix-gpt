c18fabb17ba91ece8bc75d71c64c8ae200659d9e
sai-sahoo <mail2saisahoo@gmail.com>
2023-12-18 07:56:23 +0530

    delete cons

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'XXXXXXXXXXX'
-    }
-};
-export const TMDB_MOVIE_POSTER = 'https://image.tmdb.org/t/p/w500/';
